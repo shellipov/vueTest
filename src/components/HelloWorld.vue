1ccfb0abf879de7c76ec7cc5c8386e04e94feb0b
Шелипов Дмитрий <dshelipov@cloudfactory.ru>
2025-12-03 15:49:52 +0300

    initial commit 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
+import { ref, onMounted, onUnmounted } from 'vue';
+import { fromEvent } from 'rxjs';
 
 const count = ref(0)
+const btn = ref(null);
+const secondCount = ref(0);
+
+let subscription = null;
+
+onMounted(() => {
+  // создаём Observable на событие клика
+  const click$ = fromEvent(btn.value, 'click');
+
+  // подписываемся на событие
+  subscription = click$.subscribe(() => {
+    console.log('Кнопка нажата!');
+
+    secondCount.value += 1;
+  });
+});
+
+onUnmounted(() => {
+  // обязательно отписываемся
+  subscription?.unsubscribe();
+});
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div class="card">
+    <button ref="btn" >rxJs -  {{ secondCount }}</button>
+  </div>
 </template>
 
 <style scoped>
