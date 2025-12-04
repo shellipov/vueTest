d948bcf45b506990c429f94492cb6285662dc864
Шелипов Дмитрий <dshelipov@cloudfactory.ru>
2025-12-04 14:18:34 +0300

    fix code & add ci

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,44 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue';
-import { fromEvent } from 'rxjs';
+import { fromEvent, Subscription } from 'rxjs';
 
-const count = ref(0)
-const btn = ref(null);
+const count = ref(0);
 const secondCount = ref(0);
+const btn = ref<HTMLButtonElement | null>(null);
 
-let subscription = null;
+let subscription: Subscription | null = null;
 
 onMounted(() => {
-  // создаём Observable на событие клика
+  if (!btn.value) return;
+
   const click$ = fromEvent(btn.value, 'click');
 
-  // подписываемся на событие
   subscription = click$.subscribe(() => {
-    console.log('Кнопка нажата!');
-
+    console.log('Кнопка нажата через RxJS!');
     secondCount.value += 1;
   });
 });
 
 onUnmounted(() => {
-  // обязательно отписываемся
   subscription?.unsubscribe();
 });
-
 </script>
 
 <template>
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">
+      count is {{ count }}
+    </button>
   </div>
   <div class="card">
-    <button ref="btn" >rxJs -  {{ secondCount }}</button>
+    <button ref="btn">
+      rxJs - {{ secondCount }}
+    </button>
   </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.card {
+  margin: 10px 0;
 }
 </style>
