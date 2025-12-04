6fc5392235b3ecd11f9c296c16cacf503a9981b0
Шелипов Дмитрий <dshelipov@cloudfactory.ru>
2025-12-04 16:58:08 +0300

    aaa deploy ci

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: 'https://github.com/shellipov/vueTest',
+  base: 'https://shellipov.github.io/vueTest',
   plugins: [vue()],
 })
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: 'https://github.com/shellipov/vueTest'
+    publicPath: 'https://shellipov.github.io/vueTest'
 }
